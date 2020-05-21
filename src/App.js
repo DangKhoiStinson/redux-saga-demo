a287f41780db4e14e9fc1d6223c9b5476a8a4c95
Tran Nguyen Dang Khoi <khoi.tran@infodation.local>
2020-05-21 14:22:17 +0700

    push source code

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import reducer from "./reducers";
+import watchFetchDog from './sagas';
+import {Provider, connect} from "react-redux";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(watchFetchDog);
+const fetchDog = () => {
+  return { type: 'FETCHED_DOG' }
+};
+class Content extends React.Component {
+  render () {
+    return (
+      <div>
+        <button onClick={() => this.props.dispatch(fetchDog())}>Show Dog</button>
+          {this.props.loading 
+            ? <p>Loading...</p> 
+            : this.props.error
+                ? <p>Error, try again</p>
+                : <p><img src={this.props.url} alt='' style={{width: 500, height:'auto'}} /></p>}
+      </div>
+    )
+  }
+}
+
+const ConnectedApp = connect((state) => {
+  console.log(state);
+  return state;
+})(Content);
+
+
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <ConnectedApp />
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
